1808a5331b03514cf00e035704bfa5974882b3a0
unknown <kamblimitesh@gmail.com>
2024-05-21 02:15:02 +0530

    updated CRUD operation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from './components/Footer';
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
+import { Home } from "./components/Home";
+import LoginForm from "./components/LoginForm";
+import { NavigationBar } from "./components/NavigationBar";
+import RegistrationForm from "./components/RegistrationForm";
+import UserUpdateForm from "./components/UserUpdateForm";
+import {
+  AboutUs_Route,
+  Contactus_Route,
+  LoginForm_Route,
+  ROUTE,
+  RegistrationForm_Route,
+} from './router/AppRouter';
+import Car from "./pages/Car";
+import CarDetails from "./pages/CarDetails";
+import PaymentPage from "./pages/PaymentPage";
+import UserList from "./components/UserList"; // Import the UserList component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavigationBar />
+      <Routes>
+        <Route path={ROUTE.BASE} element={<Home />} />
+        <Route path={ROUTE.CARLIST} element={<Car />} />
+        <Route path={AboutUs_Route} element={<AboutUs />} />
+        <Route path={LoginForm_Route} element={<LoginForm />} />
+        <Route path={RegistrationForm_Route} element={<RegistrationForm />} />
+        <Route path={Contactus_Route} element={<ContactUs />} />
+        <Route path={ROUTE.CAR_DETAILS} element={<CarDetails />} />
+        <Route path={ROUTE.PAYMENT} element={<PaymentPage />} />
+        <Route path="/users" element={<UserList />} /> {/* Add route for UserList */}
+        <Route path="/users/:id/edit" element={<UserUpdateForm />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
